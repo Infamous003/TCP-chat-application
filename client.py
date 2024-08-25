f67fdb6c15f47a837236a66b61e33aeed6657440
Syed Mehdi <arfathsyed0312@gmail.com>
2024-08-25 21:21:57 +0530

    Refactor both server.py and client.py to use functions. Code is now more readable.

--- client.py
+++ client.py
@@ -4,25 +4,34 @@ FORMAT = "ascii"
 DEST_NAME = socket.gethostname()
 DEST_IP = socket.gethostbyname(DEST_NAME)
 PORT = 50005
-INITIAL_BYTE_SIZE = 1024
+BYTE_SIZE = 1024
 CONNECTED = True
 DISCONNECT_MSG = "quit"
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((DEST_IP, PORT))
-print("[CONNECTED] Client connected\n")
+def connect_client_socket():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((DEST_IP, PORT))
+    print("[CONNECTED] Client connected\n")
+    return client_socket
 
-while CONNECTED:
-    # server_response = client_socket.recv(INITIAL_BYTE_SIZE)
-    # print("[CONNECTION STATUS]: ",server_response.decode())
+def get_user_input():
     msg = input(f"{DEST_NAME}@{DEST_IP}:$ ") or "[EMPTY STRING]"
-    client_socket.send(msg.encode(FORMAT))
-    if msg != "quit":
-        print("[SENT] Message sent...\n")
-        server_reply = client_socket.recv(INITIAL_BYTE_SIZE).decode(FORMAT)
-        print(f"[SERVER]:$ {server_reply}")
-        continue
-    CONNECTED = False
+    return msg
 
-print(f"[SHUTDOWN] Client has shutdown...")
-client_socket.close()
+def main():
+    global CONNECTED
+    client_socket = connect_client_socket()
+    while CONNECTED:
+        msg = get_user_input()
+        client_socket.send(msg.encode(FORMAT))
+        if msg != "quit":
+            print("[SENT] Message sent...\n")
+            server_reply = client_socket.recv(BYTE_SIZE).decode(FORMAT)
+            print(f"[SERVER]:$ {server_reply}")
+            continue
+        else:
+            CONNECTED = False
+            break
+    print(f"[SHUTDOWN] Client has shutdown...")
+    client_socket.close()
+main()
